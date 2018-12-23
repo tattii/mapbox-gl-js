27fb7ecc5a527615ea2a1f50b543592de44177f1
tattii <yuta.tatti@gmail.com>
2018-12-23 23:26:24 +0900

    elevation calculation (x pos matix)

--- src/render/draw_relief.js
+++ src/render/draw_relief.js
@@ -17,7 +17,6 @@ function drawRelief(painter: Painter, sourceCache: SourceCache, layer: Hillshade
     if (painter.renderPass !== 'translucent') return;
 
     const context = painter.context;
-    const sourceMaxZoom = sourceCache.getSource().maxzoom;
 
     context.setDepthMode(painter.depthModeForSublayer(0, DepthMode.ReadOnly));
     context.setStencilMode(StencilMode.disabled);
@@ -32,44 +31,73 @@ function drawRelief(painter: Painter, sourceCache: SourceCache, layer: Hillshade
 }
 
 
-function getTileLatRange(painter, tileID: OverscaledTileID) {
-    const coordinate0 = tileID.toCoordinate();
-    const coordinate1 = new Coordinate(coordinate0.column, coordinate0.row + 1, coordinate0.zoom);
-    return [painter.transform.coordinateLocation(coordinate0).lat, painter.transform.coordinateLocation(coordinate1).lat];
-}
-
 function renderRelief(painter, tile, layer) {
     const context = painter.context;
     const gl = context.gl;
-    const fbo = tile.fbo;
-    if (!fbo) return;
 
-    const program = painter.useProgram('relief');
-    const posMatrix = painter.transform.calculatePosMatrix(tile.tileID.toUnwrapped(), true);
-    context.activeTexture.set(gl.TEXTURE0);
+    if (tile.dem && tile.dem.level) {
+        const tileSize = tile.dem.level.dim;
 
-    gl.bindTexture(gl.TEXTURE_2D, fbo.colorAttachment.get());
+        const pixelData = tile.dem.getPixels();
+        context.activeTexture.set(gl.TEXTURE1);
 
-    gl.uniformMatrix4fv(program.uniforms.u_matrix, false, posMatrix);
-    gl.uniform1i(program.uniforms.u_image, 0);
+        context.pixelStoreUnpackPremultiplyAlpha.set(false);
+        tile.demTexture = tile.demTexture || painter.getTileTexture(tile.tileSize);
+        if (tile.demTexture) {
+            const demTexture = tile.demTexture;
+            demTexture.update(pixelData, false);
+            demTexture.bind(gl.NEAREST, gl.CLAMP_TO_EDGE);
+        } else {
+            tile.demTexture = new Texture(context, pixelData, gl.RGBA, false);
+            tile.demTexture.bind(gl.NEAREST, gl.CLAMP_TO_EDGE);
+        }
 
-    const shadowColor = layer.paint.get("hillshade-shadow-color");
-    gl.uniform4f(program.uniforms.u_shadow, shadowColor.r, shadowColor.g, shadowColor.b, shadowColor.a);
+        context.activeTexture.set(gl.TEXTURE0);
 
-    if (tile.maskedBoundsBuffer && tile.maskedIndexBuffer && tile.segments) {
-        program.draw(
-            context,
-            gl.TRIANGLES,
-            layer.id,
-            tile.maskedBoundsBuffer,
-            tile.maskedIndexBuffer,
-            tile.segments
-        );
-    } else {
-        const buffer = painter.rasterBoundsBuffer;
-        const vao = painter.rasterBoundsVAO;
-        vao.bind(context, program, buffer, []);
-        gl.drawArrays(gl.TRIANGLE_STRIP, 0, buffer.length);
+        let fbo = tile.fbo;
+
+        if (!fbo) {
+            const renderTexture = new Texture(context, {width: tileSize, height: tileSize, data: null}, gl.RGBA);
+            renderTexture.bind(gl.LINEAR, gl.CLAMP_TO_EDGE);
+
+            fbo = tile.fbo = context.createFramebuffer(tileSize, tileSize);
+            fbo.colorAttachment.set(renderTexture.texture);
+        }
+
+        context.bindFramebuffer.set(fbo.framebuffer);
+        context.viewport.set([0, 0, tileSize, tileSize]);
+
+        const matrix = mat4.create();
+        // Flip rendering at y axis.
+        mat4.ortho(matrix, 0, EXTENT, -EXTENT, 0, 0, 1);
+        mat4.translate(matrix, matrix, [0, -EXTENT, 0]);
+
+
+        // ----------------
+        const program = painter.useProgram('relief');
+        const posMatrix = painter.transform.calculatePosMatrix(tile.tileID.toUnwrapped(), true);
+
+        gl.uniformMatrix4fv(program.uniforms.u_matrix, false, posMatrix);
+        gl.uniform1i(program.uniforms.u_image, 1);
+
+        const shadowColor = layer.paint.get("hillshade-shadow-color");
+        gl.uniform4f(program.uniforms.u_shadow, shadowColor.r, shadowColor.g, shadowColor.b, shadowColor.a);
+
+        if (tile.maskedBoundsBuffer && tile.maskedIndexBuffer && tile.segments) {
+            program.draw(
+                context,
+                gl.TRIANGLES,
+                layer.id,
+                tile.maskedBoundsBuffer,
+                tile.maskedIndexBuffer,
+                tile.segments
+            );
+        } else {
+            const buffer = painter.rasterBoundsBuffer;
+            const vao = painter.rasterBoundsVAO;
+            vao.bind(context, program, buffer, []);
+            gl.drawArrays(gl.TRIANGLE_STRIP, 0, buffer.length);
+        }
     }
 }
 
--- src/shaders/relief.fragment.glsl
+++ src/shaders/relief.fragment.glsl
@@ -1,18 +1,19 @@
 uniform sampler2D u_image;
 varying vec2 v_pos;
 
-uniform vec2 u_latrange;
-uniform vec2 u_light;
 uniform vec4 u_shadow;
-uniform vec4 u_highlight;
-uniform vec4 u_accent;
 
-#define PI 3.141592653589793
+float getElevation(vec2 coord) {
+    // Convert encoded elevation value to meters
+    vec4 data = texture2D(u_image, coord) * 255.0;
+    return (data.r + data.g * 256.0 + data.b * 256.0 * 256.0) - 65536.0;
+}
+
 
 void main() {
-    vec4 pixel = texture2D(u_image, v_pos);
+	float v = getElevation(v_pos);
 
-    gl_FragColor = u_shadow;
+    gl_FragColor =  v > 100.0 ? u_shadow : vec4(1.0, 1.0, 1.0, 1.0);
 
 #ifdef OVERDRAW_INSPECTOR
     gl_FragColor = vec4(1.0);
