ed707438dd3cc0fd0b30a8c86d295fd74ccb7192
tattii <yuta.tatti@gmail.com>
2018-12-25 05:30:45 +0900

    refactor getElevation

--- src/shaders/relief.fragment.glsl
+++ src/shaders/relief.fragment.glsl
@@ -5,28 +5,21 @@ varying vec2 v_pos;
 uniform float u_color_len;
 uniform float u_opacity;
 
-float getElevation0(vec2 coord) {
+float getElevation(sampler2D u_tex, vec2 coord, float bias) {
     // Convert encoded elevation value to meters
-    vec4 data = texture2D(u_image, coord) * 255.0;
-    return (data.r + data.g * 256.0 + data.b * 256.0 * 256.0);
-}
-
-float getElevation1(vec2 coord, float bias) {
-    // Convert encoded elevation value to meters
-    vec4 data = texture2D(u_table, coord) * 255.0;
+    vec4 data = texture2D(u_tex, coord) * 255.0;
     return (data.r + data.g * 256.0 + data.b * 256.0 * 256.0);
 }
 
 float getIndex(float v) {
     for (float i = 0.0; i < 128.0; i++){
 		if (i >= u_color_len) return u_color_len - 1.0;
-		if (getElevation1(vec2(i / u_color_len, 1), 0.0) >= v) return i;
+		if (getElevation(u_table, vec2(i / u_color_len, 1), 0.0) >= v) return i;
     }
 }
 
-
 void main() {
-    float v = getElevation0(v_pos);
+    float v = getElevation(u_image, v_pos, 0.0);
 	float i = getIndex(v);
 
 	vec4 color = texture2D(u_table, vec2(i / u_color_len, 0), 0.0);
